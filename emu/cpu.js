7f0600c5ebaaeba9bcfe45c76845644dfa9d19aa
encadyma <encadyma@gmail.com>
2019-12-12 04:35:07 -0800

    Progress made

---
+++ emu/cpu.js
@@ -0,0 +1,13 @@
+// Zilog Z80.
+// 8-bit chip (1 byte at a time)
+// 16-bit address bus
+// Programs accessed through same bus as normal memory
+// Instructions range from one-three bytes
+
+// The eternal state of the CPU.
+
+_cpu = {
+    // PROGRAM COUNTER
+    pc: 0,
+
+}
