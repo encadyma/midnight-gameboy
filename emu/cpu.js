8970b0de09f4169af5c9663e3d1efd1ce41409d6
encadyma <encadyma@gmail.com>
2019-12-12 05:10:52 -0800

    Temporary update and pause; transition to chip8?

--- emu/cpu.js
+++ emu/cpu.js
@@ -6,8 +6,55 @@
 
 // The eternal state of the CPU.
 
-_cpu = {
-    // PROGRAM COUNTER
-    pc: 0,
+CPU = {
+    // REGISTERS: Registers, each
+    // holding one byte. Used for calculation.
+    // There are other special registers,
+    // such as F(L) (flags register) and
+    // the SP (stack pointer)
+    r: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        H: 0,
+        L: 0,
 
+        // SPECIAL REGISTERS
+        // FLAG (F/FL): Four possible flags.
+        FL: 0,
+
+        // PROGRAM COUNTER: a counter
+        // that advances each time by
+        // the number of bytes per instruction ran.
+        PC: 0,
+
+        // STACK POINTER
+        SP: 0,
+
+        // PREVIOUS CLOCK
+        M: 0,
+        T: 0,
+    },
+
+    // Current clock for the Z80
+    // 1 Machine Cycle = 4 Clock Cycles
+    clock: {
+        M: 0,       // Machine cycles
+        T: 0,       // Clock cycles
+    },
+
+    // OPCODE IMPLEMENTATIONS:
+    // This will be a very long file!
+
+    NOP: function() {
+        // Takes 1 M-time
+        CPU.r.M = 1; CPU.r.T = 4;
+    },
+
+    NOIMP: function(op) {
+        // Does nothing.
+        console.log("No implementation: " + op)
+    },
 }
