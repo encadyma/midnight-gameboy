eb64c4a6466265bb5e4fd1631d9de0c84821201f
encadyma <encadyma@gmail.com>
2019-12-12 21:07:50 -0800

    Opcodes for CHIP8 implemented

--- chip8/cpu.js
+++ chip8/cpu.js
@@ -12,11 +12,13 @@ CHIP8 = {
 
         I: 0,       // ADDRESS REGISTER (16-bit)
         PC: 0,      // PROGRAM COUNTER (16-bit)
-        ST: new Uint16Array(new ArrayBuffer(64)), // THE STACK,
+        SP: 0,      // STACK POINTER (points to empty top)
 
         TD: 0,      // DELAY TIMER
         TS: 0,      // SOUND TIMER
     },
+
+    ST: new Uint16Array(new ArrayBuffer(64)), // THE STACK!
 
     // Loads in bytes from PRGMBUFFER onto 0x200-.
     memLoad(prgmBuffer) {
@@ -25,14 +27,12 @@ CHIP8 = {
         CHIP8.r.I = 0;
         CHIP8.r.PC = 0x200;
 
-        V = new Uint8Array(new ArrayBuffer(16));
-        prgm = new Uint16Array(prgmBuffer);
+        CHIP8.r.V = new Uint8Array(new ArrayBuffer(16));
+        prgm = new Uint8Array(prgmBuffer);
 
         for (let i = 0; i < prgm.length; i++) {
             CHIP8_MEM[i + 0x200] = prgm[i];
         }
-
-        drawMemory();
     },
 
     // Consumes from memory the next operation.
@@ -42,15 +42,115 @@ CHIP8 = {
         nextOp <<= 8;
         nextOp |= CHIP8_MEM[CHIP8.r.PC + 1];
         CHIP8.r.PC += 2;
-        console.log("next operation: " + hex(nextOp));
+        CHIP8.lastOp = nextOp;
         CHIP8.do(nextOp);
     },
+
+    lastOp: 0,
+    colorRaised: "#88dd00",
 
     // Performs an opcode operation
     // Needs to handle all 35 opcodes!
     do: function(op) {
-        console.error("opcode not supported: " + hex(op));
-    }
+        firstDigit = (op & 0xf000) >> 12;
+        secondDigit = (op & 0x0f00) >> 8;
+        thirdDigit = (op & 0x00f0) >> 4;
+        fourthDigit = op & 0x000f;
+
+        switch (firstDigit) {
+            case 0x0:
+                // handle 0x0..
+                CHIP8.colorRaised = "#95a5a6";
+                break;
+            case 0x1:
+                // JUMP opcode (0x1NNN)
+                CHIP8.r.PC = op & 0x0fff;
+                CHIP8.colorRaised = "#1abc9c";
+                break;
+            case 0x2:
+                // CALL SUBROUTINE (0x2NNN)
+                // Function call (advanced jump)
+                CHIP8.ST[CHIP8.r.SP] = CHIP8.r.PC;
+                CHIP8.r.SP += 1;
+                CHIP8.r.PC = op & 0x0fff;
+                CHIP8.colorRaised = "#9b59b6";
+                break;
+            case 0x3:
+                // EQUALS OP (0x3XNN)
+                // Skips next instruction
+                // if VX == NN
+                if (CHIP8.r.V[secondDigit] == op & 0xff) {
+                    CHIP8.r.PC += 2;
+                }
+                CHIP8.colorRaised = "#3B3B98";
+                break;
+            case 0x4:
+                // NOT EQUALS OP (0x4XNN)
+                // Skips next instruction
+                // if VX != NN
+                if (CHIP8.r.V[secondDigit] != op & 0xff) {
+                    CHIP8.r.PC += 2;
+                }
+                CHIP8.colorRaised = "#6D214F";
+                break;
+            case 0x5:
+                // EQUALS REG OP (0x5XY0)
+                // Skips next instruction
+                // if VX == VY
+                if (fourthDigit == 0) {
+                    if (CHIP8.r.V[secondDigit] == CHIP8.r.V[thirdDigit]) {
+                        CHIP8.r.PC += 2;
+                    }
+                    CHIP8.colorRaised = "#3B3B98";
+                    break;
+                }
+            case 0x6:
+                // LOAD TO REG (0x6XNN)
+                // Sets VX to NN.
+                CHIP8.r.V[secondDigit] = op & 0xff;
+                CHIP8.colorRaised = "#9AECDB";
+                break;
+            case 0x7:
+                // ADD TO REG (0x7XNN)
+                // Sets VX to NN.
+                CHIP8.r.V[secondDigit] += op & 0xff;
+                CHIP8.colorRaised = "#9AECDB";
+                break;
+            case 0x8:
+                // Handle math!
+                CHIP8.handleMath(fourthDigit, secondDigit, thirdDigit);
+                CHIP8.colorRaised = "#FC427B";
+                break;
+            case 0x9:
+                // NOT EQUALS REG OP (0x9XY0)
+                // Skips next instruction
+                // if VX != VY
+                if (fourthDigit == 0) {
+                    if (CHIP8.r.V[secondDigit] != CHIP8.r.V[thirdDigit]) {
+                        CHIP8.r.PC += 2;
+                    }
+                    CHIP8.colorRaised = "#3B3B98";
+                    break;
+                }
+            case 0xA:
+                // SET I opcode (0xANNN)
+                CHIP8.r.I = op & 0x0fff;
+                CHIP8.colorRaised = "#CAD3C8";
+                break;
+            case 0xB:
+                // JUMP + V opcode (0xBNNN)
+                CHIP8.r.PC = (op & 0x0fff) + CHIP8.r.V[0];
+                CHIP8.colorRaised = "#1abc9c";
+                break;
+            default:
+                CHIP8.colorRaised = "#ff0000";
+                console.error("opcode not supported: " + hex(op) + ", first digit " + firstDigit);
+        }
+    },
+
+    handleMath: function(mode, x, y) {
+        
+    },
 }
 
 // There are 0xFFF (~4096) bytes of memory here.
@@ -73,20 +173,38 @@ CHIP8_GRAPHICS = {
             }
             ctx.fillRect(x * 10, y * 10, 10, 10);
         }
+    },
+
+    clear: function() {
+        // TODO: Add more to clear.
+        CHIP8_GRAPHICS.buffer = new Uint8Array(new ArrayBuffer(64*32));
     }
 }
+
+let FPS_INTERVAL = 200;
 
 function drawMemory() {
     let ctx = document.getElementById("memview").getContext("2d");
     for (let m = 0; m < CHIP8_MEM.length; m++) {
         let x = m % 64, y = Math.floor(m / 64);
-        if (CHIP8_MEM[m] > 0) {
+        if (CHIP8.r.PC == m) {
+            ctx.fillStyle = "#ddaa00";
+        } else if (CHIP8_MEM[m] > 0) {
             ctx.fillStyle = "#00dddd";
         } else {
             ctx.fillStyle = "#dddddd";
         }
-        ctx.fillRect(x * 1, y * 1, 1, 1);
+        ctx.fillRect(x * 4, y * 4, 4, 4);
     }
+
+    // Draw Status
+    ctx.clearRect(0, 256, 256, 24);
+    ctx.fillStyle = "#ddaa00";
+    ctx.font = '14px monospace';
+    ctx.fillText('fps: ' + (1000 / FPS_INTERVAL).toFixed(2), 10, 272);
+    ctx.fillText('pc: ' + hex(CHIP8.r.PC), 100, 272);
+    ctx.fillStyle = CHIP8.colorRaised;
+    ctx.fillText('opc: ' + hex(CHIP8.lastOp), 170, 272);
 }
 
 function loadROM(fileList) {
@@ -97,10 +215,20 @@ function loadROM(fileList) {
         fileList[0].arrayBuffer().then((b) => {
             console.log("ROM read completed, size = " + new Uint8Array(b).length);
             CHIP8.memLoad(b);
+            drawMemory();
+
+            clockInterval = setInterval(loop, FPS_INTERVAL);
         })
     } else {
         console.log("change found, but no files uploaded.");
     }
+}
+
+let clockInterval;
+
+function loop() {
+    CHIP8.read();
+    drawMemory();
 }
 
 function hex(num) {
@@ -110,4 +238,10 @@ function hex(num) {
 function init() {
     CHIP8_GRAPHICS.draw();
     drawMemory();
+}
+
+function clear() {
+    if (clockInterval) {
+        clearInterval(clockInterval);
+    }
 }
---
+++ guides/chip8.md
@@ -0,0 +1,2 @@
+https://github.com/trapexit/chip-8_documentation
+https://en.wikipedia.org/wiki/CHIP-8
